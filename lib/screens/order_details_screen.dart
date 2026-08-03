4c853617eafa0b936edd9596b42566a115d7e62f
hguevara1 <hguevarasoto@gmail.com>
2026-08-03 22:42:45 +0200

    fix: finalize order lifecycle sync and weight calculation for SCRUM-192

--- lib/screens/order_details_screen.dart
+++ lib/screens/order_details_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 import '../l10n/app_localizations.dart';
 import '../models/order_model.dart';
 import '../providers/order_provider.dart';
